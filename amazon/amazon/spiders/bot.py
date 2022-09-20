bd8fc7f2e1b1e4e4dc483a35b15a4d0eec83bd5f
Akın <72783002+akinyolci@users.noreply.github.com>
2022-09-20 18:47:57 +0000

    m

--- amazon/amazon/spiders/bot.py
+++ amazon/amazon/spiders/bot.py
@@ -1,4 +1,4 @@
-#from urllib.parse import urljoin
+from urllib.parse import urljoin
 import scrapy
 from ..items import AmazonItem
 
@@ -10,6 +10,20 @@ class BotSpider(scrapy.Spider):
         'https://www.amazon.com/s?i=grocery&rh=n%3A16310101&fs=true&page=5&qid=1663505602&ref=sr_pg_3',
          #for i in 400 ?
     ]
+
+    
+
+
+
+
+    def parse_product_page(self, response):
+        asin = response.meta['asin']
+        title = response.xpath('//*[@id="productTitle"]/text()').extract_first()
+        
+        seller_rank = response.xpath('//*[text()="Amazon Best Sellers Rank:"]/parent::*//text()[not(parent::style)]').extract()
+        yield {'asin': asin, 'Title': title,'SellerRank': seller_rank}
+
+
 
     def parse(self, response):
         items = AmazonItem()
@@ -34,6 +48,33 @@ class BotSpider(scrapy.Spider):
         for price in products_prices:
             if price !='.':
                 products_prices_list.append(price)
+
+        
+
+        for asin in products_asins_list:
+            product_url = f"https://www.amazon.com/dp/{asin}"
+            yield scrapy.Request(url=product_url, callback=self.parse_product_page, meta={'asin': asin})
+        
+        next_page = response.xpath('//li[@class="a-last"]/a/@href').extract_first()
+        if next_page:
+            url = urljoin("https://www.amazon.com",next_page)
+            yield scrapy.Request(url=product_url, callback=self.parse_keyword_response)
+
+
+    def parse_keyword_response(self, response):
+        for asin in products_asins_list:
+            product_url = f"https://www.amazon.com/dp/{asin}"
+            yield scrapy.Request(url=product_url, callback=self.parse_product_page, meta={'asin': asin})
+
+
+
+
+
+
+
+
+
+
         """
         Storage = {}
         
