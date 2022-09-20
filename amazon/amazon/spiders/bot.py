3963b48a3411f5445467264de69250880a874245
Akın <72783002+akinyolci@users.noreply.github.com>
2022-09-20 06:07:13 +0000

    main

--- amazon/amazon/spiders/bot.py
+++ amazon/amazon/spiders/bot.py
@@ -16,13 +16,13 @@ class BotSpider(scrapy.Spider):
         product_name = response.css('.a-size-base-plus::text').extract()
                             #product_author = response.css('.a-color-secondary .a-size-base.s-link-style').css('::text').extract()
         product_price = response.css('.a-price-whole').css('::text').extract()
-        #product_link = response.css('div.s-result-item a::attr(data-asin)').extract()
-        products = response.xpath('//*[@data-asin]')
 
-        product_Link_list = []
-        for product in products:
+        all_products_asins = response.xpath('//*[@data-asin]')
+
+        products_Links_list = []
+        for product in all_products_asins:
             asin = product.xpath('@data-asin').extract_first()
-            product_Link_list.append( asin ) #product_url = f"https://www.amazon.com/dp/{asin}"
+            products_Links_list.append( asin ) #product_url = f"https://www.amazon.com/dp/{asin}"
 
 
 
@@ -32,7 +32,7 @@ class BotSpider(scrapy.Spider):
         items['product_name'] = product_name
         #items['product_author'] = product_author
         items['product_price'] = product_price
-        items['product_imagelink'] = product_Link_list
+        items['product_imagelink'] = products_Links_list
         #items['next_page'] = next_pag
 
         yield items
